1a52f4a1f6108108221650f535bc2596d600e12c
yoohyun-1203 <yoohyun9448@gmail.com>
2025-01-11 20:52:17 +0900

    유저스키마 수정

--- models/user_schema.js
+++ models/user_schema.js
@@ -2,11 +2,11 @@ import mongoose, { Schema, model } from "mongoose";
 import { getCurrentTime } from "../utils/utils.js";
 
 const userSchema = new Schema({
-    email : { type : String, require : true, unique : true},
+    email : { type : String, require : true},
     emailAdress : { type : String, require : true},
     password : { type : String },
     nickname : {type : String, require : true},
-    age : { type : Number, default : 0},
+    intro : { type : String, default : ""},
     phone : { type : String, default : "00000000000"},
     pictureName : {type : String, default : "none_picture.jpg"},
     picturePath : {type : String, default : "/uploads/profiles"},
