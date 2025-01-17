498b0509c7d06f23f2bf41d886abe801100d143d
codewith-yeain <dev.yeain@gmail.com>
2025-01-17 19:51:40 +0900

    전화번호 인증 후 커밋 완료

--- routes/user/userRouter.js
+++ routes/user/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, loginUser, registerUser, updateUser, updatePicture, updatePassword } from '../../controller/user/user.js';
+import { deleteUser, loginUser, registerUser, updateUser, updatePicture, updatePassword, sendVerificationCode, verifyCode } from '../../controller/user/user.js';
 
 const userRouter = express.Router();
 
@@ -9,5 +9,8 @@ userRouter.put("/update", updateUser);
 userRouter.delete("/delete", deleteUser);
 userRouter.post("/picture", updatePicture);
 userRouter.post("/change-password", updatePassword);
+// 문자 인증 관련 라우트
+userRouter.post("/send-verification", sendVerificationCode);
+userRouter.post("/verify-code", verifyCode);
 
 export default userRouter;
