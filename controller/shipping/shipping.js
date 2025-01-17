9c5174ab387d7d4dedbd5e4aa7d248f70f165263
codewith-yeain <dev.yeain@gmail.com>
2025-01-17 16:25:37 +0900

    배송지 조회 작업 중

--- controller/shipping/shipping.js
+++ controller/shipping/shipping.js
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import Shipping from "../../models/shipping_schema.js";
 import User from "../../models/user_schema.js";
 
@@ -57,4 +58,47 @@ const registerShipping = async (req, res) => {
     }
 };
 
-export { registerShipping };
+const listShipping = async (req, res) => {
+    console.log(req.body);
+    const { email } = req.body; // 쿼리 스트링에서 이메일을 받음
+
+    if (!email) {
+        return res.status(400).json({ success: false, message: "이메일이 제공되지 않았습니다." });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "해당 이메일에 해당하는 사용자를 찾을 수 없습니다.",
+        });
+    }
+
+    try {
+        // 이메일을 기준으로 배송지 목록 조회
+        console.log(user._id);
+        const shippingAddresses = await Shipping.find({ userRef: user._id });
+        console.log(shippingAddresses); // 결과 확인
+
+        if (shippingAddresses.length === 0) {
+            return res.status(204).json({
+                success: true,
+                message: '배송지가 없습니다.',
+                shippingAddresses: [],
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            shippingAddresses, // MongoDB에서 반환된 배송지 목록
+        });
+    } catch (error) {
+        console.error('Error fetching shipping list:', error);
+        return res.status(500).json({ success: false, message: '서버 오류가 발생했습니다.' });
+    }
+};
+
+
+
+export { registerShipping, listShipping };
--- routes/shipping/shippingRouter.js
+++ routes/shipping/shippingRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { registerShipping } from '../../controller/shipping/shipping.js';
+import { listShipping, registerShipping } from '../../controller/shipping/shipping.js';
 
 const shippingRouter = express.Router();
 
 shippingRouter.post("/register", registerShipping);
+shippingRouter.get("/list", listShipping);
 
 export default shippingRouter;
