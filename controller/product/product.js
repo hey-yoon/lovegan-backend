812087efcf1f0a37d275a5ad30fc197261b61e65
ChoiYunsik <ykuranus@gmail.com>
2025-01-22 00:35:10 +0900

    product router 수정

--- controller/product/product.js
+++ controller/product/product.js
@@ -1,8 +1,17 @@
 import Product from '../../models/product_schema.js'
 
 const getProduct = async (req, res) => {
+     const {categories, tag} = req.query;
    try{
-    
+        const productLists = await Product.find({ categories: categories, tag: tag });
+        res.json(productLists)
+   }catch(error){   
+        res.status(500).json({error : '서버 오류'})
+
+   }
+};
+const postProduct = async (req, res) => {
+   try{
         const productLists = await Product.find();
         res.json(productLists)
    }catch(error){   
@@ -11,4 +20,4 @@ const getProduct = async (req, res) => {
    }
 };
 
-export {getProduct}
+export {getProduct, postProduct}
--- models/product_schema.js
+++ models/product_schema.js
@@ -4,6 +4,12 @@ const productSchema = new Schema({
     title: {type : String, required : true},
     description : {type : String, required : true},
     price: {type : Number, required : true},
+    star : {type : String, required : true},
+    review : {type : Number, required : true},
+    discount : {type: Number, required : true},
+    tag : {type : String, required : true},
+    categories : {type : String, required : true},
+    image : {type : String}
 })
 
 export default model("Product", productSchema, "product");
