d887bda57be1991d2ce22a5445c960a91e9abe9f
kimhanbii <hanbe0109@gmail.com>
2025-02-08 13:42:20 +0900

    몽고디비 댓글 수정 + 스키마 약간 수정

--- models/comment_schema.js
+++ models/comment_schema.js
@@ -2,7 +2,6 @@ import { model, Schema } from "mongoose";
 import { getCurrentTime } from "../utils/utils.js";
 
 const commentSchema = new Schema({
-    // author : {type : Schema.Types.ObjectId, ref : "Post", required : true, default: "익명"},
     post: { type: Schema.Types.ObjectId, ref: 'Post', required: true }, // postId 저장
     content : { type : String, required : true }, // 댓글 내용
     createAt : { type : Date, default : getCurrentTime }, 
